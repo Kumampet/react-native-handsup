f0690a19dd528e36c936f170ce4fb0adb0cd12f1
Kumampet <kumampet@gmail.com>
2024-01-22 16:13:36 +0900

    見た目を変更

--- handson/App.js
+++ handson/App.js
@@ -1,20 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  TextInput
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} />
+          <TouchableOpacity style={styles.button} >
+            <Text style={styles.buttonText} >ボタンだよ</Text>
+          </TouchableOpacity>
+        </View>
+        <StatusBar style="light" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 20,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#222"
+  },
+  button: {
+    backgroundColor: "rgb(29, 161, 242)",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 20
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: '900',
+    fontSize: 16
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 10
+  },
+  input: {
+    flex: 1,
+    borderColor: 'rgb(29, 161, 242)',
+    borderWidth: 2,
+    marginRight: 10,
+    borderRadius: 10,
+    color: 'white',
+    paddingHorizontal: 10,
+    fontSize: 16,
+  }
 });
